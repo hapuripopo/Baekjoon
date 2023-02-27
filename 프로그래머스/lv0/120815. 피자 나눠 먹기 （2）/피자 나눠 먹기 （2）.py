125a80d0d6cc1db33e967743a6e4116d0475f496
Seonga Hong <47219181+hapuripopo@users.noreply.github.com>
2023-02-27 09:52:17 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,10 +1,3 @@
 import math
 def solution(n):
-    i = 1
-    while(True):
-        if (n * i) % 6 == 0:
-            i = n * i / 6
-            break;
-        i += 1
-        
-    return i
+    return (n * 6) // math.gcd(n, 6) // 6
